4c87fd3bc381ea560e54cbea9c383596599a5d65
Deadlegend1321 <mudit1318@outlook.com>
2024-02-29 19:05:35 +0530

    JWT

--- src/main/java/com/mudit/userservice/security/SpringSecurity.java
+++ src/main/java/com/mudit/userservice/security/SpringSecurity.java
@@ -3,6 +3,7 @@ package com.mudit.userservice.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,5 +15,10 @@ public class SpringSecurity {
         http.csrf().disable();
         http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/mudit/userservice/services/AuthService.java
+++ src/main/java/com/mudit/userservice/services/AuthService.java
@@ -6,22 +6,31 @@ import com.mudit.userservice.models.SessionStatus;
 import com.mudit.userservice.models.User;
 import com.mudit.userservice.repositories.SessionRepository;
 import com.mudit.userservice.repositories.UserRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.MacAlgorithm;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMapAdapter;
 
+import javax.crypto.SecretKey;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class AuthService {
     private UserRepository userRepository;
     private SessionRepository sessionRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public AuthService(UserRepository userRepository, SessionRepository sessionRepository) {
+    public AuthService(UserRepository userRepository, SessionRepository sessionRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.userRepository = userRepository;
         this.sessionRepository = sessionRepository;
     }
@@ -34,16 +43,31 @@ public class AuthService {
 
         User user = userOptional.get();
 
-        if(!user.getPassword().equals(password)){
+        /*if(!bCryptPasswordEncoder.matches(password, user.getPassword())){
             return null;
         }
-        String token = RandomStringUtils.randomAlphabetic(30);
+        String token = RandomStringUtils.randomAlphabetic(30);*/
+
+        Map<String, Object> jsonForJwt = new HashMap<>();
+        jsonForJwt.put("email", user.getEmail());
+        jsonForJwt.put("roles", user.getRoles());
+        jsonForJwt.put("expirationDate", new Date());
+        jsonForJwt.put("createdAt", new Date());
+
+        MacAlgorithm algorithm = Jwts.SIG.HS256;
+        SecretKey key = algorithm.key().build();
+
+        String token = Jwts.builder()
+                .claims(jsonForJwt)
+                .signWith(key, algorithm)
+                .compact();
+
         Session session = new Session();
         session.setToken(token);
         session.setUser(user);
         sessionRepository.save(session);
 
-        UserDto userDto = new UserDto();
+        UserDto userDto = User.from(user);
 
         MultiValueMapAdapter<String, String> headers = new MultiValueMapAdapter<>(new HashMap<>());
         headers.add(HttpHeaders.SET_COOKIE, "auth-token:" + token);
@@ -66,7 +90,7 @@ public class AuthService {
     public UserDto signup(String email, String password) {
         User user = new User();
         user.setEmail(email);
-        user.setPassword(password);
+        user.setPassword(bCryptPasswordEncoder.encode(password));
         User savedUser = userRepository.save(user);
         return UserDto.fromUser(savedUser);
     }
@@ -76,6 +100,12 @@ public class AuthService {
         if(sessionOptional.isEmpty()){
             return null;
         }
+
+        MacAlgorithm alg = Jwts.SIG.HS256;
+        SecretKey key = alg.key().build();
+
+        Claims claims =
+                Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload();
         return SessionStatus.ACTIVE;
     }
 
